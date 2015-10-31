cb794ff175521c30899618a79d269038f87ea191
Andy Lok <andylokon@gmail.com>
2015-10-31 19:45:08 +1100

    Improvement on functionalities

--- spotify.py
+++ spotify.py
@@ -1,29 +1,31 @@
 import spotipy.util
 import collections
 import sys
+from app_secrets import SECRETS
 from pprint import pprint
-from app_secrets import secrets
 
 
 class Spotify:
-    def __init__(self, app_client_id = "", app_client_secret = "",
-    redirect_url = "", username = "", auth = False):
+    SCOPE = "playlist-modify-private playlist-read-private"
+
+    def __init__(self, secrets, username = ""):
 
         # Auth
-        if auth:
-            token = spotipy.util.prompt_for_user_token(
+        if secrets:
+            auth_token = spotipy.util.prompt_for_user_token(
                         username = username,
-                        scope = "playlist-modify-private playlist-read-private",
-                        client_id = app_client_id,
-                        client_secret = app_client_secret,
-                        redirect_uri = redirect_url)
+                        scope = self.SCOPE,
+                        client_id = secrets["client_id"],
+                        client_secret = secrets["client_secret"],
+                        redirect_uri = secrets["redirect_uri"])
+
+            self.spotipy = spotipy.Spotify(auth = auth_token)
+            self.user_id = self.spotipy.current_user()["id"]
+
+            print("Successfully logined!")
         else:
-            token = None
-
-        self.spotipy = spotipy.Spotify(auth = token)
-        self.user_id = self.spotipy.current_user()["id"]
-
-        print("Successfully logined!")
+            self.spotipy = spotipy.Spotify()
+            self.user_id = None
 
 
     def create_playlist(self, name):
@@ -51,20 +53,34 @@ class Spotify:
         return songs
 
 
-    def search_song_top(self, string):
-        return self.search_song(string, limit = 1).items()[0]
+    def search_song_first(self, string):
+        results = self.search_song(string, limit = 1)
+        if results:
+            return results.items()[0]
+        else:
+            return None
 
 
     def songs_to_playlist(self, songs, new_playlist_name):
+        # Search tracks
+        tracks = []
+        count = 0
+        for song in songs:
+            try:
+                track_id, (artists, song_name) = self.search_song_first(song)
+            except TypeError: # current track not found
+                print("\tNot found: \"%s\"" % song)
+
+                continue
+            else:
+                count += 1
+                tracks.append(track_id)
+                print("#%02d Found: %s (%s)" % (count, song_name, artists))
+
         # Create playlist
         new_playlist_id = self.create_playlist(new_playlist_name)
 
-        # Search tracks
-        tracks = []
-        for song in songs:
-            track_id, (artists, song_name) = self.search_song_top(song)
-            print("Added: %-15s - %s" % (song_name, artists))
-            tracks.append(track_id)
+        print("Playlist Successfully created!")
 
         # Add tracks to playlist
         self.spotipy.user_playlist_add_tracks(
@@ -72,11 +88,21 @@ class Spotify:
             playlist_id = new_playlist_id,
             tracks = tracks)
 
+        print("%d songs added to playlist: %s" % (len(tracks), new_playlist_name))
+
 
 
 def main():
     if len(sys.argv) != 4:
         print("Invalid number of arguments.")
+    else:
+        (username, playlist, songs_filename) = sys.argv[1:]
+
+        sp = Spotify(SECRETS, username)
+        with open(songs_filename) as f:
+            songs = [line.rstrip() for line in f]
+
+        sp.songs_to_playlist(songs, playlist)
 
 
 if __name__ == "__main__":
